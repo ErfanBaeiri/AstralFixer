6f88e2c13e434e8a2ac9fd4b122f9ecbd50b8754
ErfanBaeiri <ErfanBaeiri@gmail.com>
2023-02-19 10:49:42 +0330

    Project implementation
    16:Changing the user's avatar using Ajax
    17:Implementation of Loading in Ajax submission

---
+++ Src/BugFixer.Application/Services/Implementations/UserService.cs
@@ -0,0 +1,205 @@
+﻿using BugFixer.Application.Generators;
+using BugFixer.Application.Security;
+using BugFixer.Application.Services.Interfaces;
+using BugFixer.Application.Statics;
+using BugFixer.Domain.Entities.Account;
+using BugFixer.Domain.Interfaces;
+using BugFixer.Domain.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace BugFixer.Application.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        #region Ctor
+
+        private readonly IUserRepository _userRepository;
+        private IEmailService _emailService;
+
+        public UserService(IUserRepository userRepository, IEmailService emailService)
+        {
+            _userRepository = userRepository;
+            _emailService = emailService;
+        }
+
+        #endregion
+
+        #region Register
+
+        public async Task<RegisterResult> RegisterUser(RegisterViewModel register)
+        {
+            // Check Email Exists
+            if (await _userRepository.IsExistsUserByEmail(register.Email.SanitizeText().Trim().ToLower()))
+            {
+                return RegisterResult.EmailExists;
+            }
+
+            // hash password
+            var password = PasswordHelper.EncodePasswordMd5(register.Password.SanitizeText());
+
+            // create user
+            var user = new User
+            {
+                Avatar = PathTools.DefaultUserAvatar,
+                Email = register.Email.SanitizeText().Trim().ToLower(),
+                Password = password,
+                EmailActivationCode = CodeGenerator.CreateActivationCode()
+            };
+
+            // Add To database
+            await _userRepository.CreateUser(user);
+            await _userRepository.Save();
+
+            #region Send Activation Email
+
+            var body = $@"
+                <div> برای فعالسازی حساب کاربری خود روی لینک زیر کلیک کنید . </div>
+                <a href='{PathTools.SiteAddress}/Activate-Email/{user.EmailActivationCode}'>فعالسازی حساب کاربری</a>
+                ";
+
+            await _emailService.SendEmail(user.Email, "فعالسازی حساب کاربری", body);
+
+            #endregion
+
+            return RegisterResult.Success;
+        }
+
+        #endregion
+
+        #region Login
+
+        public async Task<LoginResult> CheckUserForLogin(LoginViewModel login)
+        {
+            var user = await _userRepository.GetUserByEmail(login.Email.Trim().ToLower().SanitizeText());
+
+            if (user == null) return LoginResult.UserNotFound;
+
+            var hashedPassword = PasswordHelper.EncodePasswordMd5(login.Password.SanitizeText());
+
+            if (hashedPassword != user.Password)
+            {
+                return LoginResult.UserNotFound;
+            }
+
+            if (user.IsDelete) return LoginResult.UserNotFound;
+
+            if (user.IsBan) return LoginResult.UserIsBan;
+
+            if (!user.IsEmailConfirmed) return LoginResult.EmailNotActivated;
+
+            return LoginResult.Success;
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return await _userRepository.GetUserByEmail(email);
+        }
+
+        #endregion
+
+        #region Activation Email
+
+        public async Task<bool> ActivateUserEmail(string activationCode)
+        {
+            var user = await _userRepository.GetUserByActivationCode(activationCode);
+
+            if (user == null) return false;
+
+            if (user.IsBan || user.IsDelete) return false;
+
+            user.IsEmailConfirmed = true;
+            user.EmailActivationCode = CodeGenerator.CreateActivationCode();
+
+            await _userRepository.UpdateUser(user);
+            await _userRepository.Save();
+
+            return true;
+        }
+
+        #endregion
+
+        #region Forgot Password
+
+        public async Task<ForgotPasswordResult> ForgotPassword(ForgotPasswordViewModel forgotPassword)
+        {
+            var email = forgotPassword.Email.SanitizeText().Trim().ToLower();
+
+            var user = await _userRepository.GetUserByEmail(email);
+
+            if (user == null || user.IsDelete) return ForgotPasswordResult.UserNotFound;
+
+            if (user.IsBan) return ForgotPasswordResult.UserBan;
+
+            #region Send Activation Email
+
+            var body = $@"
+                <div> برای فراموشی کلمه عبور روی لینک زیر کلیک کنید . </div>
+                <a href='{PathTools.SiteAddress}/Reset-Password/{user.EmailActivationCode}'>فراموشی کلمه عبور</a>
+                ";
+
+            await _emailService.SendEmail(user.Email, "فراموشی کلمه عبور", body);
+
+            #endregion
+
+            return ForgotPasswordResult.Success;
+        }
+
+        #endregion
+
+        #region Reset Password
+
+        public async Task<ResetPasswordResult> ResetPassword(ResetPasswordViewModel reset)
+        {
+            var user = await _userRepository.GetUserByActivationCode(reset.EmailActivationCode.SanitizeText());
+
+            if (user == null || user.IsDelete) return ResetPasswordResult.UserNotFound;
+
+            if (user.IsBan) return ResetPasswordResult.UserIsBan;
+
+            var password = PasswordHelper.EncodePasswordMd5(reset.Password.SanitizeText());
+
+            user.Password = password;
+            user.IsEmailConfirmed = true;
+            user.EmailActivationCode = CodeGenerator.CreateActivationCode();
+
+            await _userRepository.UpdateUser(user);
+            await _userRepository.Save();
+
+            return ResetPasswordResult.Success;
+        }
+
+        public async Task<User> GetUserByActivationCode(string activationCode)
+        {
+            return await _userRepository.GetUserByActivationCode(activationCode.SanitizeText());
+        }
+
+        #endregion
+
+        #region User Panel
+
+        public async Task<User?> GetUserById(long userId)
+        {
+            return await _userRepository.GetUserById(userId);
+        }
+
+        public async Task ChangeUserAvatar(long userId, string fileName)
+        {
+            var user = await GetUserById(userId);
+
+            user.Avatar = fileName;
+
+            await _userRepository.UpdateUser(user);
+            await _userRepository.Save();
+        }
+
+        #endregion
+    }
+}
